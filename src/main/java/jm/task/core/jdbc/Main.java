89c68b3fd929b555efefd1b9517d37b7034542d1
kimvykz <kimslavakz@gmail.com>
2023-07-17 19:32:34 +0900

    some minor changes

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) throws SQLException {
 
 
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
 
         userDao.createUsersTable();
 
